48eaa76ff73d0496a235e284010e0ac1577107a7
fhblf <fhblf@tutanota.com>
2023-12-18 10:56:53 +0100

    changed to monospace font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:style=Regular:size=9:antialias=true:autohint=true";
+static char *font = "monospace:style=Regular:size=9:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
